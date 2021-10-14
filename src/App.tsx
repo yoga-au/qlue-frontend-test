577491d5a3aff4430c830476d553f68fe18e82ab
yoga-au <yogaagung.utama@gmail.com>
2021-10-14 15:17:40 +0700

    Add: Table with dummy data and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import styled from "styled-components";
+
+// component import
+import Table from "./components/Table";
+
 function App() {
+  const Container = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+  `;
+
+  const Title = styled.h1`
+    text-align: center;
+    font-size: 2.5rem;
+    margin-block: 1em;
+  `;
+
   return (
-    <div>
-      <h1>Qlue Test</h1>
-    </div>
+    <>
+      <Container>
+        <Title>Qlue Test</Title>
+        <Table />
+      </Container>
+    </>
   );
 }
 
